dfe0ce7f3e3d8232c99f94b0a4ab11406ed8fa07
zhonxu <xusleep@gmail.com>
2014-07-11 11:18:08 +0800

    the test of a class loader

---
+++ JVMTest/src/function/test/ClassLoaderTest.java
@@ -0,0 +1,34 @@
+package function.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ClassLoaderTest {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		ClassLoader myLoader = new ClassLoader(){
+			public Class<?> loadClass(String name) throws ClassNotFoundException{
+				try
+				{
+					String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+					InputStream is = getClass().getResourceAsStream(fileName);
+					if(is == null){
+						return super.loadClass(fileName);
+					}
+					byte[] b = new byte[is.available()];
+					is.read(b);
+					return defineClass(name, b, 0, b.length);
+				}
+				catch(IOException e)
+				{
+					throw new ClassNotFoundException();
+				}
+			}
+		};
+		Object obj = myLoader.loadClass("function.test.ClassLoaderTest").newInstance();
+		System.out.println(obj.getClass());
+		System.out.println(obj instanceof function.test.ClassLoaderTest);
+	}
+
+}
