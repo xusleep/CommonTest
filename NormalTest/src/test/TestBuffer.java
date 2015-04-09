199dc2cee0ffb6361e95c04cb0d82c83b1f2f233
Administrator <Administrator@DATVAL02BOQJRDB>
2015-04-09 15:09:17 +0800

    buffer test

--- NormalTest/src/test/TestBuffer.java
+++ NormalTest/src/test/TestBuffer.java
@@ -17,9 +17,17 @@ public class TestBuffer {
 		buf.getLong();
 		buf.get();
 		System.out.println(buf.position());
+		// 重新设置position，可以重复读取数据
+		buf.rewind();
+		System.out.println("buf.rewind(): " + buf.position());
+		System.out.println("buf.rewind(): " + buf.limit());
 		// 将未读数据 放到buffer的前面， position设置到未读数据的最末端，limit设置为compacity, 准备好写入，
 		buf.compact();
 		buf.flip();
+		System.out.println(buf.position());
+		System.out.println(buf.limit());
+		// 重新设置position，可以重复读取数据
+		buf.rewind();
 		System.out.println(buf.position());
 		System.out.println(buf.limit());
 	}
